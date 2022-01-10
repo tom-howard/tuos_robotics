461b897c0d0d463d88951831ea87e977036cfe7f
tom-howard <58332432+tom-howard@users.noreply.github.com>
2022-01-10 10:11:34 +0000

    Create ask_user.sh

---
+++ wsl/ask_user.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# adapted from https://gist.github.com/davejamesmiller/1965569
+
+ask() {
+
+    local prompt default reply
+
+    prompt='y/n'
+    default=''
+
+    while true; do
+
+	    # Ask the question (not using "read -p" as it uses stderr not stdout)
+	    echo -n "$1 [$prompt] "
+
+	    # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+	    read -r reply </dev/tty
+
+	    # Default?
+	    if [[ -z $reply ]]; then
+	        reply=$default
+	    fi
+
+	    # Check if the reply is valid
+	    case "$reply" in
+	        Y*|y*) return 0 ;;
+	        N*|n*) return 1 ;;
+	    esac
+
+    done
+
+}
