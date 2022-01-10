e2af64d0ed87d732a4911103cb7cdbffc40b95e4
tom-howard <58332432+tom-howard@users.noreply.github.com>
2022-01-10 10:23:53 +0000

    don't exclude vscodeserver dir

--- wsl/rosbackup.sh
+++ wsl/rosbackup.sh
@@ -27,7 +27,6 @@ tar --exclude='home/student/.local'    \
     --exclude='home/student/.rviz'     \
     --exclude='home/student/.ignition' \
     --exclude='home/student/.sdformat' \
-    --exclude='home/student/.vscode-server' \
     --checkpoint=.200 -cjf $rbpth -C / home/student/
 
 echo "."
