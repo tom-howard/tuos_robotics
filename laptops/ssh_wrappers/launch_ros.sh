1166be188c5e91bfe3600189f13bc7e937845c9e
Tom Howard <t.howard@sheffield.ac.uk>
2022-04-26 16:32:47 +0100

    pairing with ssh keys (seems to work)

--- laptops/ssh_wrappers/launch_ros.sh
+++ laptops/ssh_wrappers/launch_ros.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
 # CC BY-NC
 # Alex Lucas & Tom Howard, University of Sheffield
 # Copyright (c) 2022
+
+waffle_id=$(hostname)
 
 source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
@@ -9,5 +12,7 @@ source ~/catkin_ws/devel/setup.bash
 export ROS_MASTER_URI=http://$(hostname):11311
 export ROS_HOSTNAME=$(hostname)
 
-echo "launching ros..."
-roslaunch turtlebot3_bringup turtlebot3_robot.launch
+echo "[$waffle_id]: Launching ROS..."
+# roslaunch tb3_tools tb3.launch > /dev/null 2>&1
+# roslaunch tb3_tools tb3.launch >/dev/null
+roslaunch tb3_tools tb3.launch 2>/dev/null
---
+++ laptops/ssh_wrappers/robot_pairing.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# CC BY-NC
+# Alex Lucas & Tom Howard, University of Sheffield
+# Copyright (c) 2022
+
+waffle_id=$(hostname)
+laptop_id=$1
+
+echo "[$waffle_id]: Pairing Robot to dia-laptop$laptop_id..."
+echo "$laptop_id" > ~/.tb3_tools/dia_laptop
+
+# remove all ssh keys other than the one that has just been created.
+
+ssh_key_file=~/.ssh/authorized_keys
+new_key=$(grep "." $ssh_key_file | tail -1)
+rm $ssh_key_file
+echo "$new_key" > $ssh_key_file
+
+echo "[$waffle_id]: Pairing complete."
