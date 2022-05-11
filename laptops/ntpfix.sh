a43ff0194bbb1fd322c0a82257a06f6ced22272f
Tom Howard <t.howard@sheffield.ac.uk>
2022-05-11 12:25:40 +0100

    ntpfix for laptops

---
+++ laptops/ntpfix.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# 11th May 2022
+
+sudo apt install -y chrony ntpdate net-tools
+
+sudo wget -O /usr/local/bin/laptop_tsync ###TODO###
+sudo chmod +x /usr/local/bin/laptop_tsync
+
+sudo groupadd timesyncers
+sudo adduser student timesyncers
+
+sudo wget -O etc/sudoers.d/rossync https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/laptops/rossync
