79ff245ecf1f19d6997d3c82d0bb8c78f0cf6d48
Tom Howard <t.howard@sheffield.ac.uk>
2022-05-18 15:42:31 +0100

    works at a basic level

---
+++ laptops/ssh_wrappers/check_pairing_robot.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# CC BY-NC
+# Alex Lucas & Tom Howard, University of Sheffield
+# Copyright (c) 2022
+
+waffle_id=$(hostname)
+laptop_id=$1
+
+ln=~/.tb3_tools/dia_laptop
+touch $ln
+if grep -qi "$laptop_id" $ln; then
+    # robot-side pairing looks OK...
+    echo "1"
+else 
+    echo "0"
+fi
--- laptops/ssh_wrappers/robot_pairing.sh
+++ laptops/ssh_wrappers/robot_pairing.sh
@@ -4,10 +4,13 @@
 # Alex Lucas & Tom Howard, University of Sheffield
 # Copyright (c) 2022
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 waffle_id=$(hostname)
 laptop_id=$1
 
-echo "[$waffle_id]: Pairing Robot to dia-laptop$laptop_id..."
+echo -e "${GREEN}[$waffle_id]${NC}: Pairing Robot to dia-laptop$laptop_id..."
 echo "$laptop_id" > ~/.tb3_tools/dia_laptop
 
 # remove all ssh keys other than the one that has just been created:
@@ -16,4 +19,7 @@ new_key=$(grep "." $ssh_key_file | tail -1)
 rm $ssh_key_file
 echo "$new_key" > $ssh_key_file
 
-echo "[$waffle_id]: Pairing complete."
+echo -e "${GREEN}[$waffle_id]${NC}: Syncronising with NTP server..."
+sudo ntpdate ntp.ubuntu.com
+
+echo -e "${GREEN}[$waffle_id]${NC}: Pairing complete."
