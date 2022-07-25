240e6d78c3a6fc00daf1c8b7aba4b185beeae026
Tom Howard <t.howard@sheffield.ac.uk>
2022-07-25 15:58:22 +0100

    don't run if sudo fails!

--- laptops/setup2207.sh
+++ laptops/setup2207.sh
@@ -1,39 +1,41 @@
 #!/usr/bin/env bash
 
-sudo -v
-
-OLD_HOST=$(hostname)
-
-ask() {
-    local reply prompt
-    prompt='y/n'
-    echo -n "$1 [$prompt] >> "
-    read -r reply </dev/tty
-    if [[ -z $reply ]]; then
-        return 1;
-    elif [ "$reply" == "y" ] || [ "$reply" == "Y" ]; then
-        return 0;
-    else
-        return 1;
-    fi
-}
-
-nmcli c up DIA-LAB
-
-# get the last part of the IP address:
-ip_sfx=$(ip -o addr show dev "wlo1" | awk '$3 == "inet" {print $4}' | sed -r 's!/.*!!; s!.*\.!!')
-# subtract 100 to get the device number:
-LAPTOP_NO=$ip_sfx
-
-nmcli c up eduroam
-
-if ask "[INPUT]: Preparing to configure for device: dia-laptop$LAPTOP_NO. IS THIS CORRECT??"; then
-    echo "[INFO]: Updating hostname..."
-    sudo hostnamectl set-hostname dia-laptop$LAPTOP_NO
-    sudo sed -i 's/'$OLD_HOST'/dia-laptop'$LAPTOP_NO'/g' /etc/hosts
-    sudo sed -i 's/'$OLD_HOST'/dia-laptop'$LAPTOP_NO'/g' ~/.bashrc
-    echo "[INFO]: Robot configuration is now complete."
-    echo "Restart the device ASAP."
+if ! sudo -v; then
+    exit 0
 else
-    echo "[INFO]: CANCELLED."
+    OLD_HOST=$(hostname)
+
+    ask() {
+        local reply prompt
+        prompt='y/n'
+        echo -n "$1 [$prompt] >> "
+        read -r reply </dev/tty
+        if [[ -z $reply ]]; then
+            return 1;
+        elif [ "$reply" == "y" ] || [ "$reply" == "Y" ]; then
+            return 0;
+        else
+            return 1;
+        fi
+    }
+
+    nmcli c up DIA-LAB
+
+    # get the last part of the IP address:
+    ip_sfx=$(ip -o addr show dev "wlo1" | awk '$3 == "inet" {print $4}' | sed -r 's!/.*!!; s!.*\.!!')
+    # subtract 100 to get the device number:
+    LAPTOP_NO=$ip_sfx
+
+    nmcli c up eduroam
+
+    if ask "[INPUT]: Preparing to configure for device: dia-laptop$LAPTOP_NO. IS THIS CORRECT??"; then
+        echo "[INFO]: Updating hostname..."
+        sudo hostnamectl set-hostname dia-laptop$LAPTOP_NO
+        sudo sed -i 's/'$OLD_HOST'/dia-laptop'$LAPTOP_NO'/g' /etc/hosts
+        sudo sed -i 's/'$OLD_HOST'/dia-laptop'$LAPTOP_NO'/g' ~/.bashrc
+        echo "[INFO]: Robot configuration is now complete."
+        echo "Restart the device ASAP."
+    else
+        echo "[INFO]: CANCELLED."
+    fi
 fi
