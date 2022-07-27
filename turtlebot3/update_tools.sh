ba71fdae4f3247b2f15218b1d86ff5b51d56a96c
tom-howard <58332432+tom-howard@users.noreply.github.com>
2022-07-27 14:49:33 +0100

    exit not logout

--- turtlebot3/update_tools.sh
+++ turtlebot3/update_tools.sh
@@ -16,4 +16,4 @@ cd ~
 rm -f .bashrc .bash_aliases
 wget -O .bashrc https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bashrc
 wget -O .bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bash_aliases
-logout
+exit
