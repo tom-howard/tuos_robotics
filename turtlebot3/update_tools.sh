03169fd0342f0828c43fe6371842d0ca19850e5d
Tom Howard <t.howard@sheffield.ac.uk>
2022-07-28 12:43:54 +0100

    remove duplicate per-profile processes

--- turtlebot3/update_tools.sh
+++ turtlebot3/update_tools.sh
@@ -6,14 +6,9 @@ sudo rm -f diamond_tools
 sudo wget https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/diamond_tools
 sudo chmod +x diamond_tools
 
-rm -rf /tmp/profile_updates.sh
+rm -f /tmp/profile_updates.sh
 wget -O /tmp/profile_updates.sh https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/profile_updates.sh
 chmod +x /tmp/profile_updates.sh
-
-cd ~
-rm -f .bashrc .bash_aliases
-wget -O .bashrc https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bashrc
-wget -O .bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bash_aliases
 
 # run as admin
 /tmp/profile_updates.sh
