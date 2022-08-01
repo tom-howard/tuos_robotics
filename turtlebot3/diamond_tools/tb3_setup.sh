01f47b9be374e0c539d3ed3edb447707d164d503
Tom Howard <t.howard@sheffield.ac.uk>
2022-08-01 15:45:28 +0100

    final mods before deploying

--- turtlebot3/diamond_tools/tb3_setup.sh
+++ turtlebot3/diamond_tools/tb3_setup.sh
@@ -16,7 +16,6 @@ ask() {
     fi
 }
 
-# construct hostname from IP address
 # get the last part of the IP address:
 ip_sfx=$(ip -o addr show dev "wlp4s0" | awk '$3 == "inet" {print $4}' | sed -r 's!/.*!!; s!.*\.!!')
 # subtract 100 to get the device number:
--- turtlebot3/diamond_tools/update_tools.sh
+++ turtlebot3/diamond_tools/update_tools.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-cd /usr/local/bin/
-sudo rm -f diamond_tools
+sudo rm -f /usr/local/bin/diamond_tools
 
-sudo wget https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/diamond_tools
-sudo chmod +x diamond_tools
+sudo wget -O /usr/local/bin/diamond_tools https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/diamond_tools/diamond_tools
+sudo chmod +x /usr/local/bin/diamond_tools
 
 rm -f /tmp/profile_updates.sh
-wget -O /tmp/profile_updates.sh https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/profile_updates.sh
+wget -O /tmp/profile_updates.sh https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/diamond_tools/profile_updates.sh
 chmod +x /tmp/profile_updates.sh
 
 # run as admin
