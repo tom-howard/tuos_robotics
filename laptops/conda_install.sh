2df77e1ac281113d2feac0c845a2344fcc62d976
Tom Howard <t.howard@sheffield.ac.uk>
2022-08-08 13:34:44 +0100

    fixed configs when run as normal user

--- laptops/conda_install.sh
+++ laptops/conda_install.sh
@@ -5,8 +5,6 @@ wget -O ~/anaconda.sh https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-
 bash ~/anaconda.sh
 rm ~/anaconda.sh
 
-echo "auto_activate_base: false" > $HOME/.condarc
-
 sudo chgrp -R sharegroup $HOME/anaconda3
 sudo chmod 770 -R $HOME/anaconda3
 
@@ -15,22 +13,27 @@ sudo touch $rc_file
 sudo chown "$USER":sharegroup $rc_file
 echo "$(grep -A 14 "# >>> conda" ~/.bashrc)" > $rc_file
 
+wget -O /tmp/.bashrc_extras https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/laptops/.bashrc_extras
+
 configs="#!/usr/bin/env bash
+
+echo \"auto_activate_base: false\" > ~/.condarc
 
 rm -f ~/.bashrc
 cp /etc/skel/.bashrc ~/
 
-wget -O /tmp/.bashrc_extras https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/laptops/.bashrc_extras
-tmp_file=/tmp/.bashrc_extras
+echo \"\" >> ~/.bashrc
 while read line; do
     echo \"\$line\" >> ~/.bashrc
-done < \"\$tmp_file\"
+done < /tmp/.bashrc_extras
 "
 echo -e "$configs" > /tmp/conda_configs.sh
 
 chmod +x /tmp/conda_configs.sh
 cd ~
 # run as admin
+echo "Running conda configs as admin..."
 /tmp/conda_configs.sh
 # run as user
+echo "Running conda configs as 'student'..."
 sudo -i -u student "/tmp/conda_configs.sh"
