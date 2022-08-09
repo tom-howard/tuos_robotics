b9f960000a9a35d1bb23bb2b5961377c2a05aa24
Tom Howard <t.howard@sheffield.ac.uk>
2022-08-09 10:39:33 +0100

    new setup for 2022-23 waffle imaging

--- turtlebot3/diamond_tools/tb3_setup.sh
+++ turtlebot3/diamond_tools/tb3_setup.sh
@@ -21,49 +21,21 @@ ip_sfx=$(ip -o addr show dev "wlp4s0" | awk '$3 == "inet" {print $4}' | sed -r '
 # subtract 100 to get the device number:
 WAFFLE_NO=$(($ip_sfx-100))
 
-if ask "[INPUT]: Preparing to configure for robot: dia-waffle$WAFFLE_NO. IS THIS CORRECT??"; then
-    echo "[INFO]: Updating hostname..."
+if ask "[INPUT] Preparing to configure for robot: dia-waffle$WAFFLE_NO. IS THIS CORRECT??"; then
+    echo "[INFO] Updating hostname..."
     sudo hostnamectl set-hostname dia-waffle$WAFFLE_NO
     sudo sed -i 's/'$OLD_HOST'/dia-waffle'$WAFFLE_NO'/g' /etc/hosts
-    sudo sed -i 's/'$OLD_HOST'/dia-waffle'$WAFFLE_NO'/g' ~/.bashrc
-    if ask "[INPUT]: Do you want to update the OpenCR board firmware?"; then
-        if ask "[INPUT]: Is the OpenCR board connected?"; then
-            export OPENCR_PORT=/dev/ttyACM0
-            export OPENCR_MODEL=waffle_noetic
-            cd ~/device_firmware/opencr/opencr_update/
-            ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
-            cd ~
-            echo "[INFO]: OpenCR firmware *should* now be updated!"
-            sleep 5
-        else
-            echo "[INFO] Couldn't update the OpenCR board."
-        fi
-    fi
-    if ask "[INPUT]: Do you want to update the RealSense Camera firmware?"; then
-        if ask "[INPUT]: Is the camera connected?"; then
-            echo "[INFO]: Looking for available devices..."
-            rs-fw-update -l
-            if ask "[INPUT]: Is the device visible in the list above?"; then
-                echo "[INFO]: Attempting to update the camera firmware..."
-                rs-fw-update -f ~/device_firmware/realsense_d435/Signed_Image_UVC_5_12_14_50.bin
-                sleep 5
-                rs-fw-update -l
-                echo "[INFO]: The device firmware should now be listed (above) as 05.12.14.50..."
-                sleep 5
-            else
-                echo "[INFO]: Couldn't connect to the camera."
-            fi
-        else
-            echo "[INFO]: Couldn't update the camera."
-        fi
-    fi
-    echo "[INFO]: Robot configuration is now complete and the device now needs to be restarted."
-    if ask "[INPUT]: Do you want to shutdown the robot now?"; then
-        echo "[COMPLETE]: Powering off..."
+    echo "[INFO] Checking the RealSense Camera Firmware..."
+    rs-fw-update -l
+    echo "[INFO] Camera firmware version should be displayed above as 05.12.14.50... (hopefully!)"
+    sleep 5
+    echo "[COMPLETE] Robot configuration is now complete."
+    if ask "[INPUT] Do you want to shutdown the robot now?"; then
+        echo "[INFO] Powering off..."
         sudo poweroff
     else
-        echo "[COMPLETE]: Don't forget to reboot/poweroff ASAP."
+        echo "[INFO] Don't forget to reboot/poweroff ASAP."
     fi
 else
-    echo "[INFO]: CANCELLED."
+    echo "[INFO] CANCELLED."
 fi
