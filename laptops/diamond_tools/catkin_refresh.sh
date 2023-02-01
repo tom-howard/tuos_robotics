3302b74188ff1365c11aed3ab597749a8050dd16
Tom Howard <t.howard@sheffield.ac.uk>
2023-02-01 14:52:55 +0000

    add a catkin refresh for laptops

---
+++ laptops/diamond_tools/catkin_refresh.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Delete the old catkin workspace:
+rm -rf ~/catkin_ws/
+
+# Then, make a new one:
+mkdir -p ~/catkin_ws/src
+
+# Clone the COM2009 repo and build:
+cd ~/catkin_ws/src/
+git clone https://github.com/tom-howard/COM2009.git
+cd ~/catkin_ws/ && catkin build
