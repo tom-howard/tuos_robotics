9e72c7755f1694c7f693a5c91e3755420709db4d
Tom Howard <t.howard@sheffield.ac.uk>
2023-09-19 14:47:40 +0100

    archive old wsl_ros scripts and replace with single new one

--- wsl/archive/rosbackup.sh
+++ wsl/archive/rosbackup.sh
@@ -1,15 +1,33 @@
-#!/bin/bash
-mkdir -p /mnt/u/wsl-ros
+#!/usr/bin/env bash
 
-touch /mnt/u/wsl-ros/wsl-ros-log.txt
-echo "$(date): Backup from $(hostname) [wsl-ros version: $WSL_ROS_VER]" >> /mnt/u/wsl-ros/wsl-ros-log.txt
+if [[ "$rbname" == "" ]]; then
+  echo "You can't run this script standalone. Use 'wsl_ros backup' instead."
+  exit 1
+fi
 
-tar --exclude='/home/student/.local' \
-    --exclude='/home/student/.cache' \
-    --exclude='/home/student/.config'\
-    --exclude='/home/student/.atom'  \
-    --exclude='/home/student/.wsl_ros_ver' \
-    --exclude='/home/student/.vscode-server' \
-    --exclude='/home/student/mdk-200720' \
-    --exclude='/home/student/mdk' \
-    -cvjf /mnt/u/wsl-ros/ros-backup.tar.gz /home/student
+if [ ! -f ~/.backup_exclusions ]; then
+  # no exclusions file exists in ~, so load a default:
+  cp ~/.wsl-ros/default_backup_exclusions ~/.backup_exclusions
+fi
+
+rbpth=$rbname'.tar.gz'
+rbwpth=$(sed "s/\/mnt\/u/U\:/;s/\//\\\/g" <<< $rbpth)
+
+echo "Backing up your WSL-ROS environment to '$rbwpth', please wait..."
+
+rbdir=$(sed "s/\/[^/]*$//" <<< $rbname)
+rblog=$rbname'-log.txt'
+
+mkdir -p $rbdir
+
+if ! touch $rblog; then
+  echo "Restore failed (cannot access the backup file)."
+  exit 1
+fi
+
+if tar -X $HOME/.backup_exclusions --exclude='home/student/.wsl-ros' --checkpoint=.200 -cjf $rbpth -C / home/student ; then
+  echo "$(date): Backup from $(hostname) [wsl-ros version: $WSL_ROS_VER]" >> $rblog
+  echo -e ".\nBackup complete."
+else
+  echo -e "An error occurred during the backup process...\nYOUR DATA MAY NOT HAVE BEEN BACKED UP CORRECTLY."
+fi
--- wsl/archive/rosrestore.sh
+++ wsl/archive/rosrestore.sh
@@ -1,5 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "$(date): Restore to $(hostname) [wsl-ros version: $WSL_ROS_VER]" >> /mnt/u/wsl-ros/wsl-ros-log.txt
+rbpth=$rbname'.tar.gz'
+rbwpth=$(sed "s/\/mnt\/u/U\:/;s/\//\\\/g" <<< $rbpth)
 
-tar -xvjf /mnt/u/wsl-ros/ros-backup.tar.gz -C /
+echo "Restoring your home directory from '$rbwpth', please wait..."
+
+rblog=$rbname'-log.txt'
+
+if ! touch $rblog; then
+  echo "Restore failed (cannot access the backup file)."
+  exit 1
+fi
+
+if tar --checkpoint=.200 -xjf $rbpth -C / ; then
+  echo "$(date): Restore to $(hostname) [wsl-ros version: $WSL_ROS_VER]" >> $rblog
+  echo -e ".\nRestore complete."
+else
+  echo -e "An error occurred during the restore process...\nYOUR DATA MAY NOT HAVE BEEN RECOVERED CORRECTLY."
+fi
