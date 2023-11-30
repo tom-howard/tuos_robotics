e1458f6bde24c8d3177de4ad135b54af634b33f6
Tom Howard <t.howard@sheffield.ac.uk>
2023-11-30 12:11:08 +0000

    add dummy wsl_ros and waffle CLIs

--- turtlebot3/diamond_tools/update_tools.sh
+++ turtlebot3/diamond_tools/update_tools.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-sudo rm -f /usr/local/bin/diamond_tools
+files="/usr/local/bin/diamond_tools /usr/local/bin/wsl_ros /usr/local/bin/waffle"
+sudo rm -f $files
 
 sudo wget -O /usr/local/bin/diamond_tools https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/diamond_tools/diamond_tools
-sudo chmod +x /usr/local/bin/diamond_tools
+sudo wget -O /usr/local/bin/waffle https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/waffle
+sudo wget -O /usr/local/bin/wsl_ros https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/wsl_ros
+sudo chmod +x $files
 
 rm -f /tmp/profile_updates.sh
 wget -O /tmp/profile_updates.sh https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/diamond_tools/profile_updates.sh
