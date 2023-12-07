546868959a90fc346c72c7c992b37e5be0311845
Tom Howard <t.howard@sheffield.ac.uk>
2023-12-07 11:01:10 +0000

    use bashrc from skel and add the additional lines

--- turtlebot3/diamond_tools/profile_updates.sh
+++ turtlebot3/diamond_tools/profile_updates.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-cd ~
-rm -f .bashrc .bash_aliases
-wget -O .bashrc https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bashrc
-wget -O .bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bash_aliases
+rm -f ~/.bash_aliases ~/.bashrc
+wget -O ~/.bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bash_aliases
+cp /etc/skel/.bashrc ~/
+
+wget -O /tmp/.bashrc_extras https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bashrc_extras
+tmp_file=/tmp/.bashrc_extras
+while IFS= read -r line; do
+  echo "$line" >> ~/.bashrc
+done < "$tmp_file"
