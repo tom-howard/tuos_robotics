1988e3f55e2b0d93c777e30edc5dda7c8f115885
Tom Howard <t.howard@sheffield.ac.uk>
2023-12-08 11:14:38 +0000

    pad bash extras with new lines

--- turtlebot3/diamond_tools/profile_updates.sh
+++ turtlebot3/diamond_tools/profile_updates.sh
@@ -4,9 +4,11 @@ rm -f ~/.bash_aliases ~/.bashrc
 wget -O ~/.bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bash_aliases
 cp /etc/skel/.bashrc ~/
 
+echo "" >> ~/.bashrc
 tmp_file=~/.bashrc_extras
 wget -O $tmp_file https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bashrc_extras
 while IFS= read -r line; do
   echo "$line" >> ~/.bashrc
 done < "$tmp_file"
 rm -f $tmp_file
+echo "" >> ~/.bashrc
