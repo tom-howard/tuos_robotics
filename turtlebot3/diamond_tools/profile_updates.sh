7eb2bda00255f41dadd95d18fef363933cb70dde
Tom Howard <t.howard@sheffield.ac.uk>
2023-12-08 11:11:18 +0000

    bash extras to user profile, not tmp

--- turtlebot3/diamond_tools/profile_updates.sh
+++ turtlebot3/diamond_tools/profile_updates.sh
@@ -4,8 +4,9 @@ rm -f ~/.bash_aliases ~/.bashrc
 wget -O ~/.bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bash_aliases
 cp /etc/skel/.bashrc ~/
 
-wget -O /tmp/.bashrc_extras https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bashrc_extras
-tmp_file=/tmp/.bashrc_extras
+tmp_file=~/.bashrc_extras
+wget -O $tmp_file https://raw.githubusercontent.com/tom-howard/tuos_robotics/main/turtlebot3/.bashrc_extras
 while IFS= read -r line; do
   echo "$line" >> ~/.bashrc
 done < "$tmp_file"
+rm -f $tmp_file
