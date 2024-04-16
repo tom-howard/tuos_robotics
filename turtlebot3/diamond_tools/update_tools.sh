f5b662d48f5bb145927f5699a797c39b8264bf52
Tom Howard <t.howard@sheffield.ac.uk>
2024-04-16 14:45:05 +0100

    dont apt update & upgrade

--- turtlebot3/diamond_tools/update_tools.sh
+++ turtlebot3/diamond_tools/update_tools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt upgrade -y
+# sudo apt update && sudo apt upgrade -y
 
 files="/usr/local/bin/diamond_tools /usr/local/bin/wsl_ros /usr/local/bin/waffle"
 sudo rm -f $files
