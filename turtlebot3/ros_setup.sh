531e1322ddf6c21d9ea7bad754546a5cb346b97e
Tom Howard <t.howard@sheffield.ac.uk>
2024-09-20 12:49:50 +0100

    add LDS_MODEL

--- turtlebot3/ros_setup.sh
+++ turtlebot3/ros_setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-export TURTLEBOT3_MODEL=waffle
 source /opt/ros/humble/setup.bash
 source $HOME/tb3_ws/install/local_setup.bash
+
+export TURTLEBOT3_MODEL=waffle
+export LDS_MODEL=LDS-01
 export WAFFLE_NO=$(cat ~/.tuos/waffle_number 2>/dev/null)
 
 source /usr/share/colcon_cd/function/colcon_cd.sh
