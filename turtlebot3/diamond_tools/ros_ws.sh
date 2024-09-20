3949943960a9a45db2709b2c2b0b1323fbf634f8
Tom Howard <t.howard@sheffield.ac.uk>
2024-09-20 13:06:22 +0100

    name changes and timestamp downloads

--- turtlebot3/diamond_tools/ros_ws.sh
+++ turtlebot3/diamond_tools/ros_ws.sh
@@ -2,21 +2,21 @@
 
 rm -rf ~/tb3_ws/
 
-# Make a new catkin workspace:
+# Make a new workspace:
 mkdir -p ~/tb3_ws/src
 
 # Clone the TB3 repo:
 cd ~/tb3_ws/src
 git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
 cd ~/tb3_ws/src/turtlebot3
-rm -r turtlebot3_description/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
+rm -rf turtlebot3_cartographer turtlebot3_navigation2 turtlebot3_example
 
 # Clone the tuos_ros repo
 cd ~/tb3_ws/src/
-git clone https://github.com/tom-howard/tuos_ros.git
+git clone -b humble https://github.com/tom-howard/tuos_ros.git
 cd ~/tb3_ws/src/tuos_ros/
 rm -rf tuos_examples/ com2009_simulations/ tuos_simulations/
-cd ~/tb3_ws/ && catkin build
+cd ~/tb3_ws/ && colcon build --symlink-install
 
 # copy the pre-built Catkin Workspace over for use by 'robot':
 sudo rm -rf /home/robot/tb3_ws/
