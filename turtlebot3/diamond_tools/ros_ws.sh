1147e4a381f78e6cb7686c13ddf1e705fcefc539
Tom Howard <t.howard@sheffield.ac.uk>
2024-09-27 11:59:21 +0100

    use shared folder for ROS workspace

--- turtlebot3/diamond_tools/ros_ws.sh
+++ turtlebot3/diamond_tools/ros_ws.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
-rm -rf ~/tb3_ws/
+ROS2_WS="/home/ros/tb3_ws"
+
+rm -rf $ROS2_WS
 
 # Make a new workspace:
-mkdir -p ~/tb3_ws/src
+mkdir -p $ROS2_WS/src
 
 # Clone the TB3 repo:
-cd ~/tb3_ws/src
+cd $ROS2_WS/src
 git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
-cd ~/tb3_ws/src/turtlebot3
+cd $ROS2_WS/src/turtlebot3
 rm -rf turtlebot3_cartographer turtlebot3_navigation2 turtlebot3_example
 
 # Clone the tuos_ros repo
-cd ~/tb3_ws/src/
+cd $ROS2_WS/src/
 git clone -b humble https://github.com/tom-howard/tuos_ros.git
-cd ~/tb3_ws/src/tuos_ros/
+cd $ROS2_WS/src/tuos_ros/
 rm -rf tuos_examples/ com2009_simulations/ tuos_simulations/
-cd ~/tb3_ws/ && colcon build --symlink-install
 
-# copy the pre-built Catkin Workspace over for use by 'robot':
-sudo rm -rf /home/robot/tb3_ws/
-sudo cp -r ~/tb3_ws/ /home/robot/
-sudo chmod -R 777 /home/robot/tb3_ws/
+cd $ROS2_WS/ && colcon build --symlink-install
