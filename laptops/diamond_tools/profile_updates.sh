8c05a1305e26a699d33ade64ea26c9b0b7fc49a6
Tom Howard <t.howard@sheffield.ac.uk>
2024-10-04 14:04:45 +0100

    fix wget path

--- laptops/diamond_tools/profile_updates.sh
+++ laptops/diamond_tools/profile_updates.sh
@@ -3,7 +3,7 @@
 mkdir -p $HOME/.tuos/diamond_tools/
 
 rm -f $HOME/.bash_aliases $HOME/.bashrc $HOME/.tuos/waffle_setup.sh
-wget -O $HOME/.bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/humble/turtlebot3/.bash_aliases
+wget -O $HOME/.bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/humble/laptops/.bash_aliases
 wget -O $HOME/.tuos/waffle_setup.sh https://raw.githubusercontent.com/tom-howard/tuos_robotics/humble/laptops/waffle_setup.sh
 cp /etc/skel/.bashrc $HOME/
 
