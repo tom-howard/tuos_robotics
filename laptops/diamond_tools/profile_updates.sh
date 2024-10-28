b6afe0552bf369b5468a365ab257cdb412d67566
Tom Howard <t.howard@sheffield.ac.uk>
2024-10-28 12:36:41 +0000

    quiet wget

--- laptops/diamond_tools/profile_updates.sh
+++ laptops/diamond_tools/profile_updates.sh
@@ -3,8 +3,8 @@
 mkdir -p $HOME/.tuos/diamond_tools/
 
 rm -f $HOME/.bash_aliases $HOME/.bashrc $HOME/.tuos/tuos_laptop_setup.sh
-wget -O $HOME/.bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/humble/laptops/.bash_aliases
-wget -O $HOME/.tuos/tuos_laptop_setup.sh https://raw.githubusercontent.com/tom-howard/tuos_robotics/humble/laptops/tuos_laptop_setup.sh
+wget -qO $HOME/.bash_aliases https://raw.githubusercontent.com/tom-howard/tuos_robotics/humble/laptops/.bash_aliases
+wget -qO $HOME/.tuos/tuos_laptop_setup.sh https://raw.githubusercontent.com/tom-howard/tuos_robotics/humble/laptops/tuos_laptop_setup.sh
 cp /etc/skel/.bashrc $HOME/
 
 echo "" >> $HOME/.bashrc
