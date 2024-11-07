1c373933d889868658f936f8a0f14bc7fb4de7a0
Tom Howard <t.howard@sheffield.ac.uk>
2024-11-07 14:47:55 +0000

    uncomment the important bits

--- turtlebot3/diamond_tools/ros_ws.sh
+++ turtlebot3/diamond_tools/ros_ws.sh
@@ -3,16 +3,16 @@
 ROS_WS="/home/ros/tb3_ws"
 REPO_DIR="/home/ros/repos"
 
-# rm -rf $ROS_WS
-# # Make a new workspace:
-# mkdir -p $ROS_WS/src
+rm -rf $ROS_WS
+# Make a new workspace:
+mkdir -p $ROS_WS/src
 
-# cd $REPO_DIR/turtlebot3/
-# # git pull --quiet
-# tb3_pkgs=("bringup" "description" "node" "teleop")
-# for tb3_pkg in ${tb3_pkgs[@]}; do
-#   cp -r turtlebot3_$tb3_pkg $ROS_WS/src/
-# done
+cd $REPO_DIR/turtlebot3/
+# git pull --quiet
+tb3_pkgs=("bringup" "description" "node" "teleop")
+for tb3_pkg in ${tb3_pkgs[@]}; do
+  cp -r turtlebot3_$tb3_pkg $ROS_WS/src/
+done
 
 cd $REPO_DIR/tuos_ros/
 git pull --quiet
@@ -22,5 +22,5 @@ for tuos_pkg in ${tuos_pkgs[@]}; do
   cp -r tuos_$tuos_pkg $ROS_WS/src/
 done
 
-# cd $ROS_WS/ && colcon --log-level ERROR build --symlink-install
-cd $ROS_WS/ && colcon --log-level ERROR build --packages-select tuos_tb3_tools --symlink-install
+cd $ROS_WS/ && colcon --log-level ERROR build --symlink-install
+# cd $ROS_WS/ && colcon --log-level ERROR build --packages-select tuos_tb3_tools --symlink-install
