dae04c11d301992febaab4f87cc9bb0adf2f4ace
Tom Howard <t.howard@sheffield.ac.uk>
2025-07-14 13:50:17 +0100

    Add docker

--- laptops/laptop_installer.sh
+++ laptops/laptop_installer.sh
@@ -97,6 +97,41 @@ if [ ! -f $HOME/checkpoint1 ]; then
         sudo apt update -y
         sudo apt install -y git
 
+        echo -e "\n### Installing Docker ###\n"
+        # Add Docker's official GPG key:
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        # Add the repository to Apt sources:
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt update
+        sudo apt install -y docker-ce \
+                            docker-ce-cli \
+                            containerd.io \
+                            docker-buildx-plugin \
+                            docker-compose-plugin
+        sudo groupadd docker
+        sudo usermod -aG docker ${USER}
+        sudo usermod -aG docker ${student_user}
+
+        echo -e "\n### Installing NVIDIA Drivers and Container Toolkit ###\n"
+        sudo apt install -y nvidia-driver-570
+        curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+        && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+        sudo apt update
+        export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
+        sudo apt install -y nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+                            nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+                            libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+                            libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+        sudo nvidia-ctk runtime configure --runtime=docker
+        sudo systemctl restart docker
+
         # Set locales
         locale  # check for UTF-8
         sudo apt update && sudo apt install locales
