d7f3859b23cb2746121008559cb44112f11e87ba
Tom Howard <t.howard@sheffield.ac.uk>
2025-07-16 16:34:58 +0100

    New approach for the ros2 wrapper?

--- laptops/tuos_laptop_setup.sh
+++ laptops/tuos_laptop_setup.sh
@@ -13,20 +13,23 @@ fi
 export WAFFLE_NO=$(cat $HOME/.diamond/waffle_number 2>/dev/null)
 export WAFFLE_IP="192.168.139.1$(printf "%02d" "${WAFFLE_NO}")"
 
-# Check the content of robot_mode file and set RDS and LHOST_ONLY variables accordingly
+# Check the content of robot_mode file and set ENV VARS accordingly
 if [ ! -f $HOME/.diamond/robot_mode ]; then
     echo "robot" > $HOME/.diamond/robot_mode
 fi 
 
 if grep -qi "robot" $HOME/.diamond/robot_mode; then
     export RMW_IMPLEMENTATION=rmw_zenoh_cpp
-    export ROS_DOMAIN_ID=$WAFFLE_NO    
+    export ROS_DOMAIN_ID=$WAFFLE_NO
+    MODE="robot"    
 elif grep -qi "sim" $HOME/.diamond/robot_mode; then
-    export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+    unset RMW_IMPLEMENTATION
     export ROS_DOMAIN_ID=1
+    MODE="sim"
 else
     echo "Unsupported robot mode set in file '${HOME}/.diamond/robot_mode'."
-    echo "Please set it to 'robot' or 'sim'."
+    echo "Please set it to 'robot' or 'sim' using the robot_mode command."
+    MODE="unknown"
 fi
 
 export ZENOH_CONFIG_OVERRIDE="mode='client';connect/endpoints=['tcp/${WAFFLE_IP}:7447']" 
@@ -52,10 +55,14 @@ colcon() {
 ros2() {
     # check to see if zenoh is running before running ros2 commands
     # (and then do what exactly...?)
-    if pgrep -x "rmw_zenohd" > /dev/null; then
-        echo "zenoh is running."
-    else
-        echo "zenoh is not running."
+    if [[ "$MODE" == "robot" ]]; then
+        if ! pgrep -x "rmw_zenohd" > /dev/null; then
+            echo "Zenoh Middleware doesn't appear to be running."
+            echo "This must be running in order to establish/maintain a connection with your robot."
+            echo "Run the following command to launch it:"
+            echo " > ros2 run rmw_zenoh_cpp rmw_zenohd"
+            return 1
+        fi
     fi
     command ros2 "$@"
 }
