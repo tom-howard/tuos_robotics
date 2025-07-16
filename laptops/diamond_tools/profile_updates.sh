85b04a8f1a030616548bc92dd214796f010b6814
Tom Howard <t.howard@sheffield.ac.uk>
2025-07-16 16:41:40 +0100

    Rename bash config file for dia-laptops

--- laptops/diamond_tools/profile_updates.sh
+++ laptops/diamond_tools/profile_updates.sh
@@ -4,12 +4,12 @@ SRC_DIR="/home/ros/repos/tuos_robotics/laptops"
 
 mkdir -p $HOME/.diamond/diamond_tools/
 
-rm -f $HOME/.bash_aliases $HOME/.bashrc $HOME/.diamond/tuos_laptop_setup.sh
+rm -f $HOME/.bash_aliases $HOME/.bashrc $HOME/.diamond/dia-laptop-config.sh
 cd $SRC_DIR
 cp bash_aliases $HOME/.bash_aliases
-cp tuos_laptop_setup.sh $HOME/.diamond/
+cp dia-laptop-config.sh $HOME/.diamond/
 cp /etc/skel/.bashrc $HOME/
 
 echo "" >> $HOME/.bashrc
-echo "source $HOME/.diamond/tuos_laptop_setup.sh" >> $HOME/.bashrc
+echo "source $HOME/.diamond/dia-laptop-config.sh" >> $HOME/.bashrc
 echo "" >> $HOME/.bashrc
