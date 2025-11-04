29156f314dc10b64248c3b6bebf6dcb4829b311f
Tom Howard <t.howard@sheffield.ac.uk>
2025-11-04 10:00:05 +0000

    Create a local (empty) workspace

--- turtlebot3/diamond_tools/profile_updates.sh
+++ turtlebot3/diamond_tools/profile_updates.sh
@@ -2,14 +2,15 @@
 
 SRC_DIR="/home/ros/repos/tuos_robotics/turtlebot3"
 
-mkdir -p $HOME/.diamond/diamond_tools/
+mkdir -p ${HOME}/.diamond/diamond_tools/
+mkdir -p ${HOME}/ros2_ws/src
 
-rm -f $HOME/.bash_aliases $HOME/.bashrc $HOME/.diamond/dia-waffle-config.sh
+rm -f ${HOME}/.bash_aliases ${HOME}/.bashrc ${HOME}/.diamond/dia-waffle-config.sh
 cd $SRC_DIR
-cp bash_aliases $HOME/.bash_aliases 
-cp dia-waffle-config.sh $HOME/.diamond/
-cp /etc/skel/.bashrc $HOME/
+cp bash_aliases ${HOME}/.bash_aliases 
+cp dia-waffle-config.sh ${HOME}/.diamond/
+cp /etc/skel/.bashrc ${HOME}/
 
-echo "" >> $HOME/.bashrc
-echo "source $HOME/.diamond/dia-waffle-config.sh" >> $HOME/.bashrc
-echo "" >> $HOME/.bashrc
+echo "" >> ${HOME}/.bashrc
+echo "source ${HOME}/.diamond/dia-waffle-config.sh" >> ${HOME}/.bashrc
+echo "" >> ${HOME}/.bashrc
