86be08c44aed59b7da5ff50a397a9b2ab04c3ceb
Tom Howard <t.howard@sheffield.ac.uk>
2025-11-04 09:59:20 +0000

    Cleanup anbd source a local ws if it exists

--- turtlebot3/dia-waffle-config.sh
+++ turtlebot3/dia-waffle-config.sh
@@ -5,6 +5,11 @@ ROS_VER=jazzy
 source /opt/ros/${ROS_VER}/setup.bash
 source /home/ros/tb3_ws/install/local_setup.bash
 
+LOCAL_WS_SETUP=${HOME}/ros2_ws/install/local_setup.bash
+if [ -f "${LOCAL_WS_SETUP}" ]; then
+  source ${LOCAL_WS_SETUP}
+fi
+
 export TURTLEBOT3_MODEL=waffle
 export LDS_MODEL=LDS-01
 export WAFFLE_NO=$(hostname | tr -d -c 0-9)
@@ -16,22 +21,3 @@ source /usr/share/colcon_cd/function/colcon_cd-argcomplete.bash
 source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
-
-# tb3_bringup() {
-#   ZENOH_ID=$(pgrep zenoh)
-  
-#   if [[ $ZENOH_ID ]]; then
-#     pkill zenoh
-#   fi
-
-#   echo "Launching a Zenoh Bridge..."
-#   sleep 1
-#   zenoh-bridge-ros2dds &
-  
-#   ROS_ARGS=$2
-
-#   echo "Launching ROS [ ros2 launch tuos_tb3_tools ros.launch.py ${ROS_ARGS} ]"
-#   sleep 1
-#   ros2 launch tuos_tb3_tools ros.launch.py ${ROS_ARGS}
-  
-# }
