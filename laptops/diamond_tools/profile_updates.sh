9e1c7db618fb5da62094bab1c74b0e52613ca2f7
Tom Howard <t.howard@sheffield.ac.uk>
2025-11-06 11:56:27 +0000

    Add completion message

--- laptops/diamond_tools/profile_updates.sh
+++ laptops/diamond_tools/profile_updates.sh
@@ -13,3 +13,5 @@ cp /etc/skel/.bashrc $HOME/
 echo "" >> $HOME/.bashrc
 echo "source $HOME/.diamond/dia-laptop-config.sh" >> $HOME/.bashrc
 echo "" >> $HOME/.bashrc
+
+echo "Profile updates applied for ${USER}. Please restart the terminal or run 'source ~/.bashrc' to apply the changes."
