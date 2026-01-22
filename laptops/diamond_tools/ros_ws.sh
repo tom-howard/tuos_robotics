e3c681f60a58ddc851591b7d2797e13b6496e54d
Tom Howard <t.howard@sheffield.ac.uk>
2026-01-22 16:10:59 +0000

    Include offer_holder pkg and handle DLs if necessary

--- laptops/diamond_tools/ros_ws.sh
+++ laptops/diamond_tools/ros_ws.sh
@@ -9,11 +9,26 @@ if id -nG "${USER}" | grep -qw "sudo"; then
   rm -rf ${ROS2_WS}
   # Re-make the workspace:
   mkdir -p ${ROS2_WS}/src
-  cd ${SHARE_DIR}/repos/tuos_ros && git pull --quiet
-  # Copy the tuos_ros repo
+  
+  # TUOS_ROS
+  if [ ! -d ${SHARE_DIR}/repos/tuos_ros ]; then
+    git clone -b jazzy https://github.com/tom-howard/tuos_ros.git ${SHARE_DIR}/repos/tuos_ros
+  else
+    cd ${SHARE_DIR}/repos/tuos_ros && git pull --quiet
+  fi
+  # Copy tuos_ros to the ros workspace
   cp -r ${SHARE_DIR}/repos/tuos_ros ${ROS2_WS}/src/
-
-  cd ${ROS2_WS}/ && colcon --log-level ERROR build --packages-up-to tuos_ros
+  
+  # com_offer_holder_days
+  if [ ! -d ${SHARE_DIR}/repos/com_offer_holder_days ]; then
+    git clone https://github.com/tom-howard/com_offer_holder_days.git ${SHARE_DIR}/repos/com_offer_holder_days
+  else
+    cd ${SHARE_DIR}/repos/com_offer_holder_days && git pull --quiet
+  fi
+  cp -r ${SHARE_DIR}/repos/com_offer_holder_days ${ROS2_WS}/src/
+  
+  # Build the workspace
+  cd ${ROS2_WS}/ && colcon --log-level ERROR build 
 else
   echo "Skipped 'tuos_ros' updates."
   mkdir -p ${HOME}/ros2_ws/src/
