4a8c1bb6cbb13e094e82cff9e2a5d1b45791ec4f
Tom Howard <t.howard@sheffield.ac.uk>
2026-03-06 22:13:53 +0000

    update odcs if user is offer_holders

--- laptops/diamond_tools/profile_updates.sh
+++ laptops/diamond_tools/profile_updates.sh
@@ -10,6 +10,10 @@ cp bash_aliases $HOME/.bash_aliases
 cp dia-laptop-config.sh $HOME/.diamond/
 cp /etc/skel/.bashrc $HOME/
 
+if [ "$USER" = "offer_holders" ]; then
+    cd $HOME/opendaycybersecurity && git pull --quiet
+fi
+
 echo "" >> $HOME/.bashrc
 echo "source $HOME/.diamond/dia-laptop-config.sh" >> $HOME/.bashrc
 echo "" >> $HOME/.bashrc
