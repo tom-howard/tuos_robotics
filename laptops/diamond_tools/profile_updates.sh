05f41dc8a69a2dbc420db6cd82b5d3adefeb13ca
Tom Howard <t.howard@sheffield.ac.uk>
2026-03-07 09:19:50 +0000

    check for repo then git pull

--- laptops/diamond_tools/profile_updates.sh
+++ laptops/diamond_tools/profile_updates.sh
@@ -10,8 +10,9 @@ cp bash_aliases $HOME/.bash_aliases
 cp dia-laptop-config.sh $HOME/.diamond/
 cp /etc/skel/.bashrc $HOME/
 
-if [ "$USER" = "offer_holders" ]; then
-    cd $HOME/opendaycybersecurity && git pull --quiet
+OD_REPO=${HOME}/opendaycybersecurity
+if [ -d "${OD_REPO}" ]; then
+  cd ${OD_REPO} && git pull --quiet
 fi
 
 echo "" >> $HOME/.bashrc
