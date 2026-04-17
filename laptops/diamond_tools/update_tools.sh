7c2a617f5cf33f7ff62e60b23dccdd1f4e73ab71
Tom Howard <t.howard@sheffield.ac.uk>
2026-04-17 12:04:05 +0100

    Sys update and install duo

--- laptops/diamond_tools/update_tools.sh
+++ laptops/diamond_tools/update_tools.sh
@@ -6,9 +6,15 @@ SRC_DIR="/home/ros/repos/tuos_robotics/laptops"
 echo "$(hostname | tr -d -c 0-9)" > /home/ros/laptop_number
 cp /home/ros/laptop_number /home/ros/waffle_number
 
-# echo "Updating system..."
-# sleep 4
-# sudo apt update && sudo apt upgrade -y
+echo "Updating system..."
+sleep 4
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Duo Desktop..."
+sleep 4
+curl -o ${HOME}/duo-desktop-latest.amd64.deb https://desktop.pkg.duosecurity.com/duo-desktop-latest.amd64.deb
+sudo dpkg -i ${HOME}/duo-desktop-latest.amd64.deb
+rm ${HOME}/duo-desktop-latest.amd64.deb
 
 echo "Updating TUoS Scripts..."
 sleep 4
